42425ce450448d4f29a7c1a4b37324c542359dab
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2020-12-15 11:10:57 +0100

    Histoire a generer

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ let name = [
     "Mikasa",
     "Armin"
 ];
-const randomName = name[Math.floor(Math.random() * name.length)];
+
 let objet = [
     " une Eppé",
     " un Equipement Tridimentionelle",
     " un Balais",
     " une Hache"
 ];
-const randomObjet = objet[Math.floor(Math.random() * objet.length)];
+
 let temperature = [
     "10°",
     "20°",
@@ -20,7 +20,7 @@ let temperature = [
     "5°",
     "0°"
 ];
-const randomTemperature = temperature[Math.floor(Math.random() * temperature.length)];
+
 let places = [
     " à la Zone d'entrainement ",
     " aux District de Trost ",
@@ -28,7 +28,7 @@ let places = [
     " à l'Exterieur des murs ",
     " aux District de Stohese "
 ];
-const randomPlaces = places[Math.floor(Math.random() * places.length)];
+
 let verbs = [
     "Se Bat contre les Titans",
     "S'entraine",
@@ -36,15 +36,18 @@ let verbs = [
     "Se Bat contre le Colosal",
     "Se Bat contre le Bestial"
 ];
-const randomVerbs = verbs[Math.floor(Math.random() * verbs.length)];
 
 let nameUser = document.getElementById('name');
 
 let history = document.getElementById('history');
 let button = document.getElementById('button');
-button.addEventListener('click', historyCreated);
 
-function historyCreated(){
-    history.innerHTML = randomName + " avec " + randomObjet + " a une temperature de " + randomTemperature + " se trouve " + randomPlaces + randomVerbs + " ! "
-        + " Cette Histoire a ete generer par " + nameUser.value;
-}
+button.addEventListener('click', function (){
+    history.innerHTML +=  name[Math.floor(Math.random() * name.length)] + " avec " + objet[Math.floor(Math.random() * objet.length)]
+        + " a une temperature de " + temperature[Math.floor(Math.random() * temperature.length)] + " se trouve "
+        + places[Math.floor(Math.random() * places.length)] + verbs[Math.floor(Math.random() * verbs.length)]
+        + " ! " + " Cette Histoire a ete generer par " + nameUser.value + '<br>';
+});
+
+
+
